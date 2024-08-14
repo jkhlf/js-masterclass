c065d91b09979f54d435dd6775f53b01790e3c47
Khalaf <joao.khalaf@gmail.com>
2024-08-13 21:07:47 -0300

    diagonalDifference and plusMinus

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 function simpleArraySum(ar) {
     var sum = 0;
 
-    for(index = 0; index < ar.length; index ++){
+    for (index = 0; index < ar.length; index++) {
         sum += ar[index];
     }
     return sum;
@@ -10,13 +10,13 @@ function simpleArraySum(ar) {
 console.log(simpleArraySum([1, 2, 3, 10]));
 
 function compareTriplets(a, b) {
-    var alice = 0 
+    var alice = 0
     var bob = 0
 
-    for(index = 0; index < a.length; index += 1 ){
-        if (a[index] > b[index] ){
-            alice ++;
-        } else if (a[index] = b [index]) {
+    for (index = 0; index < a.length; index += 1) {
+        if (a[index] > b[index]) {
+            alice++;
+        } else if (a[index] = b[index]) {
             bob += 1;
         }
     }
@@ -33,4 +33,36 @@ function aVeryBigSum(ar) {
 
     return sum;
 
+}
+
+
+function diagonalDifference(arr) {
+    var left = 0;
+    var right = 0;
+
+    for (var index = 0; index < arr.length; index += 1) {
+        left += arr[index][index];
+        right += arr[index][arr.length - 1 - index]
+    }
+
+
+    return Math.abs(left.right);
+}
+
+
+function plusMinus(arr) {
+
+    const { biggerThanZero, lessThanZero, zero } = arr.reduce((acc, num) => {
+        if (num > 0) acc.biggerThanZero += 1;
+        else if (num < 0) acc.lessThanZero += 1;
+        else acc.zero += 1;
+
+        return acc;
+    }, { biggerThanZero: 0, lessThanZero: 0, zero: 0 });
+
+    const arrLength = arr.length;
+
+    console.log((biggerThanZero / arrLength).toFixed(6));
+    console.log((lessThanZero / arrLength).toFixed(6));
+    console.log((zero / arrLength).toFixed(6));
 }
