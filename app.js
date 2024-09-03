e354769c322f4b189486a0331d86cd91e364bf20
Khalaf <joao.khalaf@gmail.com>
2024-09-03 20:34:29 -0300

    countApplesAndOranges

--- app.js
+++ app.js
@@ -206,3 +206,28 @@ function gradingStudents(grades) {
 
     return roundedGrades;
 }
+
+
+////////////////////////////////////////////////////////
+
+function countApplesAndOranges(s, t, a, b, apples, oranges) {
+    let appleCount = 0;
+    let orangeCount = 0;
+
+    for (let i = 0; i < apples.length; i++) {
+        let applePosition = a + apples[i]; 
+        if (applePosition >= s && applePosition <= t) {
+            appleCount++;
+        }
+    }
+
+    for (let i = 0; i < oranges.length; i++) {
+        let orangePosition = b + oranges[i];
+        if (orangePosition >= s && orangePosition <= t) {
+            orangeCount++;
+        }
+    }
+
+    console.log(appleCount);
+    console.log(orangeCount);
+}
